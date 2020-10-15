c02da47b6b5b3e208c78805e3730535b9c1aeb25
Kiril <mikasaxd@gmai.com>
2020-10-15 15:01:56 +0300

    work with obj

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
 "use strict";// режим строгого современного кода
 
-let incr = 10;
-let decr = 10;
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+//console.log(numberOfFilms);
+
+const personalMovieDb = {
+
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
 
 
-console.log(incr++);
-console.log(decr--);
 
+const question1= prompt("Один из последних просмотренных фильмов?", "");
+const question2= +prompt("На сколько оцените его?", "");
+const question3= prompt("Один из последних просмотренных фильмов?", "");
+const question4= +prompt("На сколько оцените его?", "");
+
+
+personalMovieDb.movies[question1] = question2;
+personalMovieDb.movies[question3] = question4;
+console.log(personalMovieDb);
